49314631ddbbce36ef411f886078a1213e3e5d84
Norbert Goździk <norbert1303@wp.pl>
2022-06-26 16:53:01 +0200

    Initial commit

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+const {override} = require('customize-cra');
+const {aliasWebpack, configPaths} = require('react-app-alias-ex');
+
+module.exports = {
+    webpack: override(
+        aliasWebpack(configPaths('./tsconfig.paths.json'))
+    ),
+}
+
